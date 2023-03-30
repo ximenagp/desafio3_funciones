977994834414fe5c62758e407852cfb6c0079c00
ximenagp <ximenagp@gmail.com>
2023-03-30 17:57:08 -0300

    uno

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,10 @@
+/* Transforma esta declaración de función a una función de expresión. El ejercicio debe
+quedar en un archivo nombrado `1 funcion.js`*/
+
+function example(a, b, c){
+    return a+b+c
+    }
+//Funcion de expresión:
+const example = function example(a,b,c) {
+    return a+b+c;
+}
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,9 @@
+/*Transforma las siguiente función a una arrow function de una línea. Este ejercicio
+debe quedar en un archivo llamado `2 arrow.js`*/
+
+suma = function(a, b){
+return a + b
+}
+
+//2_arrow
+const suma = (a,b)=> a+b+c;
